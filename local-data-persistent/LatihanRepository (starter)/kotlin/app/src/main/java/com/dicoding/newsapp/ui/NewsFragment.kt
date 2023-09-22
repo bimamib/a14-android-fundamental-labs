62114bb8903d80b65dedead4807c0380ed1144e0
bimamib <bimaprast1601@gmail.com>
2023-09-23 04:32:20 +0700

    menginisiasi ViewModel dan menampilkan data

--- local-data-persistent/LatihanRepository (starter)/kotlin/app/src/main/java/com/dicoding/newsapp/ui/NewsFragment.kt
+++ local-data-persistent/LatihanRepository (starter)/kotlin/app/src/main/java/com/dicoding/newsapp/ui/NewsFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.newsapp.databinding.FragmentNewsBinding
 
@@ -23,7 +25,40 @@ class NewsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         tabName = arguments?.getString(ARG_TAB)
+
+        val factory: ViewModelFactory = ViewModelFactory.getInstance(requireActivity())
+        val viewModel: NewsViewModel by viewModels {
+            factory
+        }
+
         val newsAdapter = NewsAdapter()
+
+        if (tabName == TAB_NEWS) {
+            viewModel.getHeadlineNews().observe(viewLifecycleOwner) { result ->
+                if (result != null) {
+                    when (result) {
+                        is com.dicoding.newsapp.data.Result.Loading -> {
+                            binding?.progressBar?.visibility = View.VISIBLE
+                        }
+
+                        is com.dicoding.newsapp.data.Result.Success -> {
+                            binding?.progressBar?.visibility = View.GONE
+                            val newsData = result.data
+                            newsAdapter.submitList(newsData)
+                        }
+
+                        is com.dicoding.newsapp.data.Result.Error -> {
+                            binding?.progressBar?.visibility = View.GONE
+                            Toast.makeText(
+                                context,
+                                "Terjadi kesalahan" + result.error,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+        }
         
         binding?.rvNews?.apply {
             layoutManager = LinearLayoutManager(context)
